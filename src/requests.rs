a4b24537a256410815e4d947216230634bf55d61
Rail Khusnutdinov <mail@rail-ka.ru>
2021-03-06 21:12:29 +0300

    ...

--- src/requests.rs
+++ src/requests.rs
@@ -10,25 +10,28 @@ errors:
 409.1003: treasure is not digged
 */
 
+// /health-check
 pub fn health_check() {}
 
+// /balance
 pub fn balance() -> Balance {}
 
+// /licenses
 pub fn licenses() -> LicenseList {}
 
-// post
+// post /licenses
 // пустой массив для получения бесплатной лицензии
 // errors: 409.1002: no more active licenses allowed
 pub fn licenses_set(body: Wallet) -> License {}
 
-// post
+// post /explore
 // Returns amount of treasures in the provided area at full depth.
 // args: Area to be explored
 // return: Report about found treasures.
 // errors: 422.1000: wrong coordinates
 pub fn explore(body: Area) -> Report {}
 
-// post
+// post /dig
 // Dig at given point and depth, returns found treasures.
 // args: License, place and depth to dig.
 // return: List of treasures found.
@@ -36,7 +39,7 @@ pub fn explore(body: Area) -> Report {}
 // 422.1001: wrong depth
 pub fn dig(body: Dig) -> TreasureList {}
 
-// post
+// post /cash
 // Exchange provided treasure for money.
 // args: Treasure for exchange.
 // return: Payment for treasure.
